6d4a16edd2044c8097315dfa90dd7b8a802cd972
Arquma <aaronquma@gmail.com>
2015-05-26 23:41:02 -0600

    primer commit

---
+++ Rutica/src/Rutica/DataBaseBundle/DataBaseBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rutica\DataBaseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DataBaseBundle extends Bundle
+{
+}
---
+++ Rutica/src/Rutica/RutasBundle/RutasBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rutica\RutasBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RutasBundle extends Bundle
+{
+}
