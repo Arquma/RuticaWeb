fc2a1195f25aaeb856c7f46d1f57d2059e227cc1
jorgeucr <jorge.castillovindas@gmail.com>
2015-07-03 00:15:41 -0600

    Funcionalidad: Ultimos sitios ingresados. AdministradorHome. Completa

--- Rutica/app/cache/dev/twig/fb/b5/ad6c7b8a5379a157eb0fbbf7d488a93d6735d00a4dff501d056f7b13f0aa.php
+++ Rutica/app/cache/dev/twig/fb/b5/ad6c7b8a5379a157eb0fbbf7d488a93d6735d00a4dff501d056f7b13f0aa.php
@@ -18,30 +18,35 @@ class __TwigTemplate_fbb5ad6c7b8a5379a157eb0fbbf7d488a93d6735d00a4dff501d056f7b1
         // line 2
         $this->env->loadTemplate("AdministradorBundle:Default:adminHeader.html.twig")->display($context);
         // line 3
-        echo " <div class=\"collapse navbar-collapse\">
-                <ul class=\"nav navbar-nav navbar-right\">
-                    <li class=\"active\"><a href=\"";
-        // line 5
+        echo "
+
+<div class=\"collapse navbar-collapse\">
+    <ul class=\"nav navbar-nav navbar-right\">
+        <li class=\"active\"><a href=\"";
+        // line 7
         echo $this->env->getExtension('routing')->getUrl("ainicio");
         echo "\">Inicio</a></li>
-                    
-                    <li ><a href=\"";
-        // line 7
+
+        <li ><a href=\"";
+        // line 9
         echo $this->env->getExtension('routing')->getUrl("sitio");
         echo "\">Sitios turísticos</a></li>
-                    <li ><a href=\"";
-        // line 8
+        <li ><a href=\"";
+        // line 10
         echo $this->env->getExtension('routing')->getUrl("rutasugerida");
         echo "\">Rutas</a></li>
-                    
-                    <li><a href=\"contactenos.html\">Salir</a></li>                    
-                </ul>
-            </div>
 
-        </div>
-    </header><!--/header-->
-    <br><br>
-    
+        <li><a href=\"contactenos.html\">Salir</a></li>                    
+    </ul>
+</div>
+
+</div>
+</header><!--/header-->
+<br><br>
+
+
+
+
 <!-- Sección de los nuevos sitios turísticos -->\t
 
 <div class=\"section section-white\">
@@ -53,97 +58,113 @@ class __TwigTemplate_fbb5ad6c7b8a5379a157eb0fbbf7d488a93d6735d00a4dff501d056f7b1
             </div>
 
 
-            <ul class=\"grid cs-style-3\">
-                <div class=\"col-md-4 col-sm-6\">
-                    <figure>
-                        <img src=\"";
-        // line 32
-        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/templateboostrap/img/sitios/1.jpg"), "html", null, true);
-        echo "\" alt=\"\" />
-                        <figcaption>
-                            <h3>Bosque nuboso</h3>
-                            <h3>monteverde</h3>
-                            <span>Puntarenas</span>\t\t\t\t\t\t\t
-                            <a href=\"#ventanaModalDescripcionSitio\" class=\"btn btn-primary\" data-toggle=\"modal\">Visítalo</a>
-                        </figcaption>
-                    </figure>
-                </div>\t
-                <div class=\"col-md-4 col-sm-6\">
-                    <figure>
-                        <img src=\"";
-        // line 43
-        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/templateboostrap/img/sitios/2.jpg"), "html", null, true);
-        echo "\" alt=\"\" />
-                        <figcaption>
-                            <h3>Playa Nosara</h3>
-                            <span>Guanacaste</span>
-                            <a href=\"#ventanaModalDescripcionSitio\" class=\"btn btn-primary\" data-toggle=\"modal\">Visítalo</a>
-                        </figcaption>
-                    </figure>
-                </div>
-                <div class=\"col-md-4 col-sm-6\">
-                    <figure>
-                        <img src=\"";
-        // line 53
-        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/templateboostrap/img/sitios/3.jpg"), "html", null, true);
-        echo "\" alt=\"\" />
-                        <figcaption>
-                            <h3>Parque Nacional</h3>
-                            <h3>Marino Ballena</h3>
-                            <span>Puntarenas</span>
-                            <a href=\"#ventanaModalDescripcionSitio\" class=\"btn btn-primary\" data-toggle=\"modal\">Visítalo</a>
-                        </figcaption>
-                    </figure>
-                </div>
-                <div class=\"col-md-4 col-sm-6\">
-                    <figure>
-                        <img src=\"";
-        // line 64
-        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/templateboostrap/img/sitios/4.jpg"), "html", null, true);
-        echo "\" alt=\"\" />
-                        <figcaption>
-                            <h3>Parque Recreativo</h3>
-                            <h3>Los Chorros</h3>
-                            <span>Alajuela</span>
-                            <a href=\"#ventanaModalDescripcionSitio\" class=\"btn btn-primary\" data-toggle=\"modal\">Visítalo</a>
-                        </figcaption>
-                    </figure>
-                </div>
-                <div class=\"col-md-4 col-sm-6\">
-                    <figure>
-                        <img src=\"";
-        // line 75
-        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/templateboostrap/img/sitios/5.jpg"), "html", null, true);
-        echo "\" alt=\"\" />
-                        <figcaption>
-                            <h3>Parque Nacional Tortuguero</h3>
-                            <span>Limón</span>
-                            <a href=\"#ventanaModalDescripcionSitio\" class=\"btn btn-primary\" data-toggle=\"modal\">Visítalo</a>
+            <ul class=\"grid cs-style-2\"> 
+                ";
+        // line 35
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["ultimoSitios"]) ? $context["ultimoSitios"] : $this->getContext($context, "ultimoSitios")));
+        foreach ($context['_seq'] as $context["_key"] => $context["entity"]) {
+            // line 36
+            echo "                    <div class=\"col-md-4 col-sm-6\">
+                        <figure>
+                            ";
+            // line 38
+            $context["foo"] = ("imagenes_sitios/" . $this->getAttribute($context["entity"], "imagen", array()));
+            echo "   
 
-                        </figcaption>
-                    </figure>
-                </div>
-                <div class=\"col-md-4 col-sm-6\">
-                    <figure>
-                        <img src=\"";
-        // line 86
-        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/templateboostrap/img/sitios/6.jpg"), "html", null, true);
-        echo "\" alt=\"\" />
-                        <figcaption>
-                            <h3>Playa Cocal</h3>
-                            <span>Puntarenas</span>
-                            <a href=\"#ventanaModalDescripcionSitio\" class=\"btn btn-primary\" data-toggle=\"modal\">Visítalo</a>
-                        </figcaption>
-                    </figure>
-                </div>
-            </ul>
+                            <img src=\"";
+            // line 40
+            echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl((isset($context["foo"]) ? $context["foo"] : $this->getContext($context, "foo"))), "html", null, true);
+            echo "\" alt=\"\" height=\"250px\" width=\"400px\" href=\"#ventanaModalDescripcionSitio\" data-toggle=\"modal\" data-test=\"";
+            echo twig_escape_filter($this->env, $this->getAttribute($context["entity"], "imagen", array()), "html", null, true);
+            echo "\" 
+                                 data-desc=\"";
+            // line 41
+            echo twig_escape_filter($this->env, $this->getAttribute($context["entity"], "descripcion", array()), "html", null, true);
+            echo "\" data-nombre=\"";
+            echo twig_escape_filter($this->env, $this->getAttribute($context["entity"], "nombre", array()), "html", null, true);
+            echo "\" data-provincia=\"";
+            echo twig_escape_filter($this->env, $this->getAttribute($context["entity"], "id_provincia", array()), "html", null, true);
+            echo "\"
+                                 data-actividad=\"";
+            // line 42
+            echo twig_escape_filter($this->env, $this->getAttribute($context["entity"], "id_actividad", array()), "html", null, true);
+            echo "\" data-id=\"";
+            echo twig_escape_filter($this->env, $this->getAttribute($context["entity"], "id", array()), "html", null, true);
+            echo "\"/>
+
+                            <figcaption>
+                                <h3>";
+            // line 45
+            echo twig_escape_filter($this->env, $this->getAttribute($context["entity"], "nombre", array()), "html", null, true);
+            echo "</h3>
+                                ";
+            // line 46
+            if (($this->getAttribute($context["entity"], "id_provincia", array()) == 1)) {
+                // line 47
+                echo "                                    <span>San José</span><br>
+                                ";
+            }
+            // line 49
+            echo "                                ";
+            if (($this->getAttribute($context["entity"], "id_provincia", array()) == 2)) {
+                // line 50
+                echo "                                    <span>Alajuela</span><br>
+                                ";
+            }
+            // line 52
+            echo "                                ";
+            if (($this->getAttribute($context["entity"], "id_provincia", array()) == 3)) {
+                // line 53
+                echo "                                    <span>Cartago</span><br>
+                                ";
+            }
+            // line 55
+            echo "                                ";
+            if (($this->getAttribute($context["entity"], "id_provincia", array()) == 4)) {
+                // line 56
+                echo "                                    <span>Heredia</span><br>
+                                ";
+            }
+            // line 58
+            echo "                                ";
+            if (($this->getAttribute($context["entity"], "id_provincia", array()) == 5)) {
+                // line 59
+                echo "                                    <span>Puntarenas</span><br>
+                                ";
+            }
+            // line 61
+            echo "                                ";
+            if (($this->getAttribute($context["entity"], "id_provincia", array()) == 6)) {
+                // line 62
+                echo "                                    <span>Guanacaste</span><br>
+                                ";
+            }
+            // line 64
+            echo "                                ";
+            if (($this->getAttribute($context["entity"], "id_provincia", array()) == 7)) {
+                // line 65
+                echo "                                    <span>Limón</span><br>
+                                ";
+            }
+            // line 67
+            echo "                            </figcaption>
+                        </figure>
+                    </div>
+                ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['entity'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 71
+        echo "            </ul>
         </div>
     </div>
 </div>
 
-    
+
 ";
-        // line 100
+        // line 77
         $this->env->loadTemplate("AdministradorBundle:Default:adminFooter.html.twig")->display($context);
     }
 
@@ -159,6 +180,6 @@ class __TwigTemplate_fbb5ad6c7b8a5379a157eb0fbbf7d488a93d6735d00a4dff501d056f7b1
 
     public function getDebugInfo()
     {
-        return array (  147 => 100,  130 => 86,  116 => 75,  102 => 64,  88 => 53,  75 => 43,  61 => 32,  34 => 8,  30 => 7,  25 => 5,  21 => 3,  19 => 2,);
+        return array (  168 => 77,  160 => 71,  151 => 67,  147 => 65,  144 => 64,  140 => 62,  137 => 61,  133 => 59,  130 => 58,  126 => 56,  123 => 55,  119 => 53,  116 => 52,  112 => 50,  109 => 49,  105 => 47,  103 => 46,  99 => 45,  91 => 42,  83 => 41,  77 => 40,  72 => 38,  68 => 36,  64 => 35,  36 => 10,  32 => 9,  27 => 7,  21 => 3,  19 => 2,);
     }
 }
--- Rutica/src/Rutica/AdministradorBundle/Controller/DefaultController.php
+++ Rutica/src/Rutica/AdministradorBundle/Controller/DefaultController.php
@@ -8,6 +8,33 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AdministradorBundle:Default:inicio.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $entities = $em->getRepository('DataBaseBundle:Sitio')->findAll();
+        $sitiosRutaSugerida = $em->getRepository('DataBaseBundle:SitioRutaSugerida')->findAll();
+
+        $hostname = $this->container->getParameter('database_host');
+        $user = $this->container->getParameter('database_user');
+        $pass = $this->container->getParameter('database_password');
+        $dataBase = $this->container->getParameter('database_name');
+        mysql_connect($hostname, $user, $pass);
+        mysql_select_db($dataBase);
+
+        $request = $this->getRequest();
+        $qry = "SELECT * FROM rutica.sitio ORDER BY id DESC LIMIT 6";
+        $res = mysql_query($qry);
+
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $connection = $em->getConnection();
+        $statement = $connection->prepare("SELECT * FROM rutica.sitio ORDER BY id DESC LIMIT 6");
+      
+        $statement->execute();
+        $results = $statement->fetchAll();
+
+
+        return $this->render('AdministradorBundle:Default:inicio.html.twig', array(
+                    'ultimoSitios' => $results,
+                    'sitiosRutaSugerida' => $sitiosRutaSugerida,
+        ));
     }
 }
